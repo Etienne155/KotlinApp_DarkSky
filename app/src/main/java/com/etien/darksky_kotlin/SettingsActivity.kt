9b721b18f849fd41ad1aa8e46c96b9a059131fa3
Etienne155 <etiennem1553@gmail.com>
2019-12-06 09:44:52 -0500

    menu with settings and about

--- app/src/main/java/com/etien/darksky_kotlin/SettingsActivity.kt
+++ app/src/main/java/com/etien/darksky_kotlin/SettingsActivity.kt
@@ -1,25 +1,16 @@
 package com.etien.darksky_kotlin
 
 import android.content.Intent
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 
-import kotlinx.android.synthetic.main.activity_main.*
-
-class MainActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        setContentView(R.layout.activity_settings)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,6 +39,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        finishAffinity()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
